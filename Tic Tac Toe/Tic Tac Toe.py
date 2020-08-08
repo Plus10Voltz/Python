5fbb10796559479644a65d6dc141442ecdad2dde
BuildTools <unconfigured@null.spigotmc.org>
2020-08-09 02:35:53 +1000

    Patched bugs

--- Tic Tac Toe/Tic Tac Toe.py
+++ Tic Tac Toe/Tic Tac Toe.py
@@ -19,15 +19,17 @@ while not game.over:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if game.winner == 0 or game.winner == "0":
-                game.onClick(None, None, "player")
-                game.checkWin()
-                game.update()
-                game.aiTurn()
-                game.checkWin()
-                game.update()
                 if debug == True:
-                    print(game.winner)
-                    game.debugBoard()
+                    # print(game.winner)
+                    print(game.playerTurn)
+                    # game.debugBoard()
+                if game.playerTurn == 0:
+                    if game.onClick(None, None, "player") == True:
+                        game.checkWin()
+                        game.update()
+                        game.aiTurn()
+                        game.checkWin()
+                        game.update()
             else:
                 game.over = True
     game.update()
